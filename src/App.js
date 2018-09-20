d330ea0823a84668525f5138f08bf434d935781e
Michael McNichol <mcnichol.m@gmail.com>
2018-09-19 20:09:51 -0500

    Base Commit, CI pipeline

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import logo from "./logo.svg";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Edge</li>
+                            <li>Hosting</li>
+                            <li>Pricing</li>
+                            <li>Contact</li>
+                            <li>Login</li>
+                            <li>Register</li>
+                        </ul>
+                    </div>
+                </header>
+                <p className="App-intro">
+                    <h1 className="App-title">Leveraging today's technology to empower the decisions of tomorrow.</h1>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
